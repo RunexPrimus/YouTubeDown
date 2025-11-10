e247fb9b0014ff50e91c63238c5c9e877c793c59
Runex <runex.primus@gmail.com>
2025-11-10 17:32:27 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+BASE_URL = "https://www.hentai.name"
+SEARCH_URL = f"{BASE_URL}/search/{{slug}}/?p={{page}}"
+GALLERY_URL = f"{BASE_URL}/g/{{gallery_id}}/1/"
+IMAGE_BASE_URL = "https://pics.hentai.name"
